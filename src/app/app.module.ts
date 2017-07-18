ae911ce744d4f61d247786f3523ccaa0fe178811
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-07-19 00:00:23 +0800

    added export for the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {ErrorHandler, NgModule} from '@angular/core';
-import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {IonicErrorHandler, IonicModule} from 'ionic-angular';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {StatusBar} from '@ionic-native/status-bar';
 
-import {MyApp} from './app.component';
-import {HomePage} from '../pages/home/home';
 import {ProgressService} from '../providers/progress-service/progress-service';
 import {CustomBrowserXhr} from '../providers/custom-browser-xhr/custom-browser-xhr';
 import {HLineComponent} from '../components/h-line/h-line';
@@ -14,28 +12,27 @@ import {LoadingDivComponent} from '../components/loading-div/loading-div';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
     HLineComponent,
     LoadingComponent,
-    LoadingDivComponent
+    LoadingDivComponent,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(AppModule)
   ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     ProgressService,
-    CustomBrowserXhr
-  ]
+    CustomBrowserXhr,
+  ],
+  exports: [
+    HLineComponent,
+    LoadingComponent,
+    LoadingDivComponent,
+  ],
 })
 export class AppModule {
 }
