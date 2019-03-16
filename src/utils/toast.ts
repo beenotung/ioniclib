a1b34f361a68074b513745147044204e3938487f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-03-16 17:50:31 +0800

    allow customize default option of showToast

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,13 +1,13 @@
 import {ToastController, ToastOptions} from "ionic-angular";
 
+export let defaultToastOption: ToastOptions = {
+  dismissOnPageChange: true,
+  duration: 2000,
+};
+
 export function showToast(toastCtrl: ToastController, opts?: ToastOptions) {
+  opts = Object.assign({}, defaultToastOption, opts);
   opts = opts || {};
-  if (opts.dismissOnPageChange === undefined) {
-    opts.dismissOnPageChange = true;
-  }
-  if (opts.duration === undefined) {
-    opts.duration = 2000;
-  }
   const toast = toastCtrl.create(opts);
   return new Promise((resolve, reject) => {
     toast.onDidDismiss((data, role) => {
